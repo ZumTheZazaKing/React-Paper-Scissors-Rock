267d861e258df64d1bf0992c6378c8baa4b7dff2
ZumTheZazaKing <zahidi85543@gmail.com>
2021-07-31 22:25:55 +0800

    Finished with the designs of choices and output

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './style.css';
 import reportWebVitals from './reportWebVitals';
 
 function ChoiceMenu(props){
   
-  return <div id="choice">
+  return <div id="choice" ref={props.choiceClass}>
 
     <img name="paper" onClick={props.handleClick}/>
     <img name="scissors" onClick={props.handleClick}/>
@@ -18,12 +18,20 @@ function ChoiceMenu(props){
 
 function OutputMenu(props){
 
-  return <div>
+  return <div id="output" className="hide" ref={props.outputClass}>
 
-    <p>{props.userChoice}</p>
-    <p>{props.computerChoice}</p>
+    <div id="fight">
+      <p>You</p>
+      <img src="" name="userImage" ref={props.userImage}/>
+      <p>VS</p>
+      <img src="" name="computerImage" ref={props.computerImage}/>
+      <p>CPU</p>
+      
+    </div>
+
     <br/>
-    <p>{props.result}</p>
+    <p className="result">{props.result}</p>
+    <br/>
 
   </div>
 
@@ -37,9 +45,17 @@ function App(){
   let [wins, setWins] = useState(parseInt(localStorage.getItem("zumthezazaking_rps_wins")) || 0);
   let [defeats, setDefeats] = useState(parseInt(localStorage.getItem("zumthezazaking_rps_defeats")) || 0);
 
+  let userImage = useRef();
+  let computerImage = useRef();
+  let choiceClass = useRef();
+  let outputClass = useRef();
+
   let computerChoices = ['rock', 'paper', 'scissors'];
 
   function handleClick(e){
+    choiceClass.current.className = "hide";
+    outputClass.current.className = "";
+    
     setUserChoice(e.target.name);
 
     let randomChoice = computerChoices[Math.floor(Math.random()*computerChoices.length)];
@@ -47,43 +63,55 @@ function App(){
 
     switch(e.target.name){
       case "rock":
+        userImage.current.className = "rock";
         if(randomChoice === "paper"){
+          computerImage.current.className = "paper";
           setResult("You lose");
           setDefeats(defeats+1);
           localStorage.setItem("zumthezazaking_rps_defeats", parseInt(defeats)+1);
         } else if (randomChoice === "scissors"){
+          computerImage.current.className = "scissors";
           setResult("You win")
           setWins(wins+1);
           localStorage.setItem("zumthezazaking_rps_wins", parseInt(wins)+1);
         } else {
+          computerImage.current.className = "rock";
           setResult("Draw");
         }
         break
 
       case "paper":
+        userImage.current.className = "paper";
         if(randomChoice === "scissors"){
+          computerImage.current.className = "scissors";
           setResult("You lose");
           setDefeats(defeats+1);
           localStorage.setItem("zumthezazaking_rps_defeats", parseInt(defeats)+1);
         } else if (randomChoice === "rock"){
+          computerImage.current.className = "rock";
           setResult("You win");
           setWins(wins+1);
           localStorage.setItem("zumthezazaking_rps_wins", parseInt(wins)+1);
         } else {
+          computerImage.current.className = "paper";
           setResult("Draw")
         }
         break;
 
       case "scissors":
+        userImage.current.className = "scissors";
         if(randomChoice === "rock"){
+          computerImage.current.className = "rock";
           setResult("You lose");
           setDefeats(defeats+1);
           localStorage.setItem("zumthezazaking_rps_defeats", parseInt(defeats)+1);
         } else if (randomChoice === "paper"){
+          computerImage.current.className = "paper";
           setResult("You win");
           setWins(wins+1);
           localStorage.setItem("zumthezazaking_rps_wins", parseInt(wins)+1);
         } else {
+          computerImage.current.className = "scissors";
           setResult("Draw")
         }
         break;
@@ -101,11 +129,14 @@ function App(){
     <p>Defeats: {defeats}</p>
 
     <div id="container">
-      <ChoiceMenu handleClick={handleClick}/>
+      <ChoiceMenu handleClick={handleClick} choiceClass={choiceClass}/>
       <OutputMenu 
       userChoice={userChoice} 
       computerChoice={computerChoice}
       result={result}
+      userImage={userImage}
+      computerImage={computerImage}
+      outputClass={outputClass}
     />
     </div>
 
